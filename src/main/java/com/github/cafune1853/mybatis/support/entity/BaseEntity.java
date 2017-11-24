194cba76a222e4cfac4f48196442458b0edf55ce
黄志伟 <doggy1853@163.com>
2017-11-24 16:13:49 +0800

    BaseEntity不再支持gmtCreate/gmtModified字段，这个建议直接通过db实现，不在业务层面暴露这两个字段，避免无谓的更新操作以及错误的查询操作

--- src/main/java/com/github/cafune1853/mybatis/support/entity/BaseEntity.java
+++ src/main/java/com/github/cafune1853/mybatis/support/entity/BaseEntity.java
@@ -13,36 +13,12 @@ public abstract class BaseEntity<T extends Serializable> {
 	@Column(name = "id")
 	private T id;
 	
-	@Column(name = "gmt_create")
-	private Date GmtCreate;
-	
-	@Column(name = "gmt_modified")
-	private Date GmtModified;
-	
 	public T getId() {
 		return id;
 	}
 	
 	public BaseEntity setId(T id) {
 		this.id = id;
-		return this;
-	}
-	
-	public Date getGmtCreate() {
-		return GmtCreate;
-	}
-	
-	public BaseEntity setGmtCreate(Date gmtCreate) {
-		GmtCreate = gmtCreate;
-		return this;
-	}
-	
-	public Date getGmtModified() {
-		return GmtModified;
-	}
-	
-	public BaseEntity setGmtModified(Date gmtModified) {
-		GmtModified = gmtModified;
 		return this;
 	}
 }
